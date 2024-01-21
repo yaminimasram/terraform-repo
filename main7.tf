9bf02bb7adf662c2ead9c9b201d3b039bee69118
yaminimasram <152468275+yaminimasram@users.noreply.github.com>
2024-01-21 14:22:15 +0530

    user data apache2

---
+++ main7.tf
@@ -0,0 +1,66 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+output "ubuntu_aws__ami_id" {
+  value = ["data.aws_ami.ubuntu.id"]
+}
+
+resource "aws_instance" "instance-for-tera" {
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.allow_tls.name]
+  user_data       = <<-EOF
+                #!/bin/bash
+                sudo apt-get update -y
+                sudo apt-get install -y apache2
+                sudo systemctl start apache2
+                sudo systemctl enable apache2
+                sudo echo "This is my page" > /var/www/html/index.html
+                sudo systemctl restart apache2
+              EOF
+
+  tags = {
+    Name = "web-instance"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow for security group"
+  vpc_id      = "vpc-0ad4768b3ef5540ef"
+  tags = {
+    Name = "allow_tls"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
